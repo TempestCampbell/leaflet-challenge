90b6631a87be15631748bf3b1ea86de802722508
Tempest <tempest@MacDeth.hsd1.co.comcast.net>
2021-03-18 22:30:29 -0600

    geoJSON call

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,4 @@
-var graymap= L.tileLayer(
+var grayMap= L.tileLayer(
     "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
     {
       attribution:
@@ -10,3 +10,35 @@ var graymap= L.tileLayer(
       accessToken: API_KEY
     }
 );
+
+var mapUS = L.map("map-id", {
+    center: [
+        40, -95
+
+    ],
+    zoom:2.9
+});
+
+//Tile layer to map
+grayMap.addTo(mapUS);
+
+//earthquake geoJSON data
+d3.json(https:"//earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson", function(data){
+
+//Functions to find color and radius
+function styleInfo(feature){
+    return {
+        opacity: 1,
+        fillOpacity:1,
+
+    }
+
+}
+
+
+
+
+
+
+
+})
